2c5752fa282e2ef128f4e23d848b7e9fde6ffcaf
anthonproperty-lab <anthonproperty@gmail.com>
2026-06-16 18:44:32 +0700

    Update GuardedRoute.tsx

--- src/components/GuardedRoute.tsx
+++ src/components/GuardedRoute.tsx
@@ -1,1 +1,16 @@
+import React, { useEffect } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuthStore } from '../stores/authStore';
+import Splash from '../pages/Splash';
 
+export const GuardedRoute: React.FC = () => {
+  const { user, initialized, checkUser } = useAuthStore();
+
+  useEffect(() => {
+    if (!initialized) checkUser();
+  }, [initialized, checkUser]);
+
+  if (!initialized) return <Splash />;
+
+  return user ? <Outlet /> : <Navigate to="/login" replace />;
+};
